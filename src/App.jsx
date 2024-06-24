120c679cd795b773ca43526ff967abc2e865883c
Nirmal Rajkumar <143875715+nrk018@users.noreply.github.com>
2024-06-24 13:37:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LoadingComponent } from './components/LoadingComponent';
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <LoadingComponent />
+    </div>
+  );
+}
+
+export default App;
